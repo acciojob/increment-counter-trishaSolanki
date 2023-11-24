a15e55092b3a22ad3ef4e52a4a7d30e5fe3f1318
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-11-24 15:56:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+var counterElement = document.getElementById('counter');
+var incrementBtn = document.getElementById('increment') ;
+
+incrementBtn.addEventListener('click' , function() {
+	var currentValue = parseInt(counterElement.innerText);
+	alert('incrementValue : ' + currentValue);
+	currentValue.innerText = currentValue + 1;
+});
